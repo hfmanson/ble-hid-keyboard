ec41a6f1fa548ea1d71784c2ca34347c6de96a4b
Henri Manson <hfmanson@gmail.com>
2026-03-01 11:35:43 +0000

    use /usr/local/lib instead of /usr/lib

--- uninstall.sh
+++ uninstall.sh
@@ -2,4 +2,4 @@ set -x
 systemctl disable ble-hid-keyboard
 systemctl stop ble-hid-keyboard.service
 rm /usr/lib/systemd/system/ble-hid-keyboard.service
-rm -r /usr/lib/ble-hid-keyboard
+rm -r /usr/local/lib/ble-hid-keyboard
